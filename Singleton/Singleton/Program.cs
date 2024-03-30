ffda54e79b14ad7a0b465865a13f01199f0fb1f5
Daniel <danielnagy600@gmail.com>
2024-03-30 14:06:38 +0100

    Singleton Pattern

---
+++ Singleton/Singleton/Program.cs
@@ -0,0 +1,28 @@
+﻿//Example: Managing a database connection pool,Caching,Configuration,Resource management
+//Logger
+//Violates the single responsibility principle: How create and lifecyce management
+//IoC
+//Where there must be exactly one instance of a class
+//Thread-safe
+
+var instance1 = Logger.Instance;
+var instance2 = Logger.Instance;
+
+if (instance1 == instance2 && instance2 == Logger.Instance)
+{
+    Console.WriteLine("Instances are the same.");
+}
+
+public class Logger 
+{
+    private static readonly Lazy<Logger> lazyLogger = new Lazy<Logger>(() => new Logger());
+
+    public static Logger Instance
+    {
+        get
+        {
+           return lazyLogger.Value ;
+        }
+    }
+    protected Logger() { }
+}
